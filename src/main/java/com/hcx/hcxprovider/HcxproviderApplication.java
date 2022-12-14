b4dff91832ca1f2c30ad7139fecf4e530f951c3d
bavishya-vitraya <bavishya@vitrayatech.com>
2022-12-14 13:14:05 +0530

    deployment changes

--- src/main/java/com/hcx/hcxprovider/HcxproviderApplication.java
+++ src/main/java/com/hcx/hcxprovider/HcxproviderApplication.java
@@ -3,13 +3,20 @@ package com.hcx.hcxprovider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @SpringBootApplication
+@RestController
 public class HcxproviderApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(HcxproviderApplication.class, args);
 	}
-
+	@GetMapping("/")
+	public ResponseEntity<String> checkHcxProvider() {
+		return  ResponseEntity.ok("Test Connection");
+	}
 }
