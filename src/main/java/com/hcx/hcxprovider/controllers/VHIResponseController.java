54f5b183035293e5f490e84c361da8fd78ea1315
bavishya-vitraya <bavishya@vitrayatech.com>
2022-12-21 16:31:26 +0530

    route path changes

--- src/main/java/com/hcx/hcxprovider/controllers/VHIResponseController.java
+++ src/main/java/com/hcx/hcxprovider/controllers/VHIResponseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-@RequestMapping("/response")
+@RequestMapping("/vitrayamockprovider")
 public class VHIResponseController {
 
     @Autowired
